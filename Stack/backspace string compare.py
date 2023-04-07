ef3d7f918d919dae6965b6184783c0eef7bafb6c
AlexeiVartoumian <alex.vartoumian@gmail.com>
2023-04-07 17:18:36 +0100

    stack question and sliding window problem

---
+++ Stack/backspace string compare.py
@@ -0,0 +1,75 @@
+"""
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+ 
+
+Example 1:
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+Example 2:
+
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+Example 3:
+
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+"""
+"""
+use stack approach and merge procedure popping whenever a hash is encountered.
+ 
+        the below will look a little like the merge procedure where
+        a while loop will go through both strings and append the values
+        to a stack if a hash occurs then delete. to account for one string being longer than 
+        another two spearate while loops doing the same thing will hae to be made
+        for each string. at the end return if they are equal to each other 
+"""
+def backspaceCompare(s, t):
+
+        stacka= []
+        stackb = []
+        i = 0
+        j = 0
+        while i != len(s)-1 and j !=len(t):
+            print(s[i],t[i])
+            if s[i] == "#":
+                if stacka:
+                    stacka.pop()
+                    
+            else:
+                    stacka.append(s[i])
+                
+            if t[j] == "#":
+                if stackb:
+                    stackb.pop()
+                    
+            else:
+                    stackb.append(t[i])
+            
+            i+=1
+            j+=1
+        while i < len(s):
+            
+            if s[i] == "#":
+                if stacka:
+                    stacka.pop()
+                    
+            else:
+                stacka.append(s[i])
+            i+=1
+        while j < len(t):
+            
+            if t[j] == "#":
+                if stackb:
+                    stackb.pop()
+                    
+            else:
+                    stackb.append(t[j])
+            j+=1
+        return stacka == stackb
