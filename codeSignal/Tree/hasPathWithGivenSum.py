e244185dd4cd55972f8eebd7d17a4284f48b92a3
AlexeiVartoumian <alex.vartoumian@gmail.com>
2023-09-18 14:17:06 +0100

    conditional traversal. return root.left,cursum or root.left,cursum

---
+++ codeSignal/Tree/hasPathWithGivenSum.py
@@ -0,0 +1,151 @@
+"""
+Given a binary tree t and an integer s, determine whether there is a root to leaf path in t such that the sum of vertex values equals s.
+
+Example
+
+For
+
+t = {
+    "value": 4,
+    "left": {
+        "value": 1,
+        "left": {
+            "value": -2,
+            "left": null,
+            "right": {
+                "value": 3,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": null
+    },
+    "right": {
+        "value": 3,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": {
+                "value": -2,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": -3,
+                "left": null,
+                "right": null
+            }
+        }
+    }
+}
+and
+s = 7,
+the output should be solution(t, s) = true.
+
+This is what this tree looks like:
+
+      4
+     / \
+    1   3
+   /   / \
+  -2  1   2
+    \    / \
+     3  -2 -3
+Path 4 -> 3 -> 2 -> -2 gives us 7, the required sum.
+
+For
+
+t = {
+    "value": 4,
+    "left": {
+        "value": 1,
+        "left": {
+            "value": -2,
+            "left": null,
+            "right": {
+                "value": 3,
+                "left": null,
+                "right": null
+            }
+        },
+        "right": null
+    },
+    "right": {
+        "value": 3,
+        "left": {
+            "value": 1,
+            "left": null,
+            "right": null
+        },
+        "right": {
+            "value": 2,
+            "left": {
+                "value": -4,
+                "left": null,
+                "right": null
+            },
+            "right": {
+                "value": -3,
+                "left": null,
+                "right": null
+            }
+        }
+    }
+}
+and
+s = 7,
+the output should be solution(t, s) = false.
+
+This is what this tree looks like:
+
+      4
+     / \
+    1   3
+   /   / \
+  -2  1   2
+    \    / \
+     3  -4 -3
+There is no path from root to leaf with the given sum 7.
+"""
+
+"""
+dfs question. recursively apply the following, if we reached a NUll Node
+then go back up to last viable node. if current viable node has no children check if current accumalted sum is plus value stored in current node is equal to target sum. return true if so.
+oterhwise accumalate tsrget sum returning either left or right child sicne we want to explore all possible routes until our condition is met.
+otherwise once all is traversed return false.
+return the function which will return a type boolean.
+"""
+
+#
+# Binary trees are already defined with this interface:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+
+def solution(t, s):
+    
+    
+    
+    def dfs(root,cursum):
+        
+        if not root:
+            return False    
+    
+        elif not root.left and not root.right:
+            
+            if cursum + root.value == s:
+                return True
+        
+        else:
+            cursum+= root.value
+            return dfs(root.left,cursum) or dfs(root.right,cursum) 
+        
+        return False
+    
+    return dfs(t,0)
