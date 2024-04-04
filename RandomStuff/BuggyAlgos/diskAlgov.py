876bb75308f097c6bee86bc570cc4f454622aada
AlexeiVartoumian <alex.vartoumian@gmail.com>
2024-04-04 20:18:29 +0100

    Disk-Scheduling Algo works as per book spec

--- RandomStuff/BuggyAlgos/diskAlgov.py
+++ RandomStuff/BuggyAlgos/diskAlgov.py
@@ -1,8 +1,11 @@
-start= 189
-tracks = [49, 116, 140, 32, 61, 117, 169, 101, 193, 178]
 
-tracks2 = [32,49 ,61 , 101 , 116  , 117,140,169,178,193]
 
+start = 116
+#track3 = [55,58,39,18,90,160,150,38,184]
+#track3 = [138, 28, 126, 88,95, 66, 98, 168, 153, 55]
+#track3 =  [134, 132, 109, 5, 21,67, 12, 107, 64, 45]
+track3 = [155, 14, 118, 96,104, 172, 83, 195, 93, 43]
+#cscan is not yet correct
 def fifo(start, tracks):
     
     asum = 0
@@ -26,21 +29,32 @@ def BrutalshortestScanFirst(start, tracks):
         visited.add(marker)
     print(asum)
 
-# brutalbug depending on where start pos is we will traverse upwards and visit all sequence elements.
-#point is that when we go down we have to find trhe first element such that is smaller than starting position
-#without calculating elements until we get there. 
+
 def BrutalBugelevator(start, tracks2):
     visited = set()
     res = []
     index = 0
-    for i in range(len(tracks2)-1):
-        if tracks2[i] <start and tracks2[i+1] > start:
-            res = tracks2[:i+1:] + [start] + tracks2[i+1::]
-            index = i+1
+    tracks2.sort()
+    up = True
+    if  start < tracks2[0]:
+            res = [start] + tracks2
+                
+    elif start > tracks2[-1]:
+        res = tracks2 + [start]
+        index = len(res)-1
+        up = False
+    else:
+        for i in range(len(tracks2)-1):
+            if tracks2[i] <start and tracks2[i+1] > start:
+                res = tracks2[:i+1:] + [start] + tracks2[i+1::]
+                index = i+1
+                break
+       
     visited.add(start)
     asum = 0
-    up = True
-   
+    
+    
+    
     prev = index
     while len(visited) != len(res):
         if up:
@@ -49,6 +63,7 @@ def BrutalBugelevator(start, tracks2):
                 asum+= abs(res[prev]- res[index+1])
                 index +=1
                 prev = index
+                
                 visited.add(res[prev])
             else:
                 up = False
@@ -61,19 +76,32 @@ def BrutalBugelevator(start, tracks2):
             index-=1
     print(asum)
 
+
+
 # here I dont check if i have already visited elements because I am only going in one direction.
 # I only care about visiting all the elements after starting poisition has been inserted
 def cscanUp(start,tracks2):
     visited = set()
     res = []
     index = 0
-    for i in range(len(tracks2)-1):
-        if tracks2[i] <start and tracks2[i+1] > start:
-            res = tracks2[:i+1:] + [start] + tracks2[i+1::]
-            index = i+1
+    tracks2.sort()
+    if  start < tracks2[0]:
+            res = [start] + tracks2
+                
+    elif start > tracks2[-1]:
+        res = tracks2 + [start]
+        index = len(res)-1
+        up = False
+    else:
+        for i in range(len(tracks2)-1):
+            if tracks2[i] <start and tracks2[i+1] > start:
+                res = tracks2[:i+1:] + [start] + tracks2[i+1::]
+                index = i+1
+            
     visited.add(start)
     asum = 0
     visited.add(start)
+    print(res , " is ")
     while len(visited) != len(res):
         nextindex = ( (index+1) % len(res)) 
         print("the diff is :" , abs(res[index] - res[ nextindex]))
@@ -82,9 +110,19 @@ def cscanUp(start,tracks2):
         index = nextindex
     
     print(asum)
-    return asum       
+    return asum
         
-fifo(start,tracks)
-BrutalshortestScanFirst(start,tracks)
-BrutalBugelevator(start,tracks2)
-cscanUp(start,tracks2)
+# fifo(start,tracks)
+# BrutalshortestScanFirst(start,tracks)
+# BrutalBugelevator(start,tracks2)
+# cscanUp(start,tracks2)
+
+print("fifo")
+fifo(start,track3)
+print("ssf")
+BrutalshortestScanFirst(start,track3)
+print("scan")
+BrutalBugelevator(start,track3)
+print("c-scan")
+cscanUp(start,track3)
+
