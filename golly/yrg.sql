a9c72f76ba1608a5e81ceb3c392a60099848663c
alex vartoumain <alex.vartoumian@gmail.com>
2025-03-24 20:19:47 +0000

    hm

--- golly/yrg.sql
+++ golly/yrg.sql
@@ -1,8 +1,7 @@
 
-config from cloud.resource where cloud.type = 'aws' AND api.name='aws-s3api-get-bucket-acl' as X; 
-config from cloud.resource where cloud.type = 'aws' AND api.name='aws-organizations-account' as Y; 
-filter 'X.resource.status = "Active" and 
-        not (X.bucketName contains "cf-templates") and 
-        not () and 
-        Y.name contains "grp-rnd"'; 
+
+
+config from cloud.resource where cloud.type = 'aws' AND api.name='aws-s3api-get-bucket-acl' and resource.status = Active and (json.rule='bucketName does not contain "cf-templates" and ) as X; 
+config from cloud.resource where cloud.type = 'aws' AND api.name='aws-organizations-account' and (json.rule = name contains "grp-rnd") as Y; 
+filter '$.X.accountId == $.Y.accountId'; 
 show X;
